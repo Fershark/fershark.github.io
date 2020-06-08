776557c110aee205d0986d5f02bca8872d58fcfe
Fershark <ferchriquelme@gmail.com>
2020-06-08 00:40:10 -0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,43 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title;
+  const social = data.site.siteMetadata.social;
+  const authorName = data.site.siteMetadata.author.name;
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <Layout
+      location={location}
+      title={siteTitle}
+      social={social}
+      authorName={authorName}
+    >
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>You just hit a route that doesn&#39;t exist.</p>
     </Layout>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
+query {
+  site {
+    siteMetadata {
+      title
+      author {
+        name
+      }
+      social {
+        link
+        name
       }
     }
   }
-`
+  }
+`;
